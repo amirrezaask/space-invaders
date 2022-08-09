b62f48b988a023b6030a85b8f49b2a3bfa95913c
amirrezaask <raskarpour@gmail.com>
2022-08-09 14:16:36 +0430

    color screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,40 @@
 #include "string"
 #include "cstdlib"
 
-void Check_Error() {
+#define WINDOW_WIDTH 1600
+#define WINDOW_HEIGHT 900
+#define u8 Uint8
+
+void check_error() {
     printf("%s", SDL_GetError());
     exit(1);
 }
 
-SDL_Surface* loadBMP(std::string path) {
-    auto surface = SDL_LoadBMP(path.c_str());
-    return surface;
+SDL_Surface* load_bmp_file(std::string path, SDL_PixelFormat* format) {
+    SDL_Surface* raw_surface = SDL_LoadBMP(path.c_str());
+    SDL_Surface* optimized = SDL_ConvertSurface(raw_surface, format, 0);
+    SDL_FreeSurface(raw_surface);
+    return optimized;
 }
 
-const int WINDOW_WIDTH = 1600;
-const int WINDOW_HEIGHT =  900;
+void set_background_color_for_surface(SDL_Surface* surface, u8 r, u8 g, u8 b) {
+    if(SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF)) < 0 ) check_error();
+}
+
 
 int main() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0)
-      Check_Error();
+      check_error();
 
-  auto background = loadBMP("./hello_world.bmp");
 
-  auto window = SDL_CreateWindow("Pinball", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 
-  auto main_surface = SDL_GetWindowSurface(window);
+  SDL_Window* window = SDL_CreateWindow("Pinball", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+  
+  SDL_Surface* main_surface = SDL_GetWindowSurface(window);
 
-  if (SDL_BlitSurface(background, NULL, main_surface, NULL) < 0) Check_Error();
+  set_background_color_for_surface(main_surface, 0xFF, 0xFF, 0xFF);
+
+  SDL_UpdateWindowSurface(window);
 
   while(true) {
       SDL_Event event;
